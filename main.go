dfa706337a259ba601429fa207080e6cd4f09d1b
jose909 <rosaleayal19@hotmail.com>
2016-09-29 16:09:24 -0600

    consultar la API classify2 a buscar información de referencia real basado en criterios de búsqueda del usuario

--- main.go
+++ main.go
@@ -1,37 +1,88 @@
-
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"html/template"
-
-	_ "github.com/mattn/go-sqlite3"
+  "fmt"
+  "net/http"
+  "html/template"
+  _ "github.com/mattn/go-sqlite3"
   "database/sql"
+  "encoding/json"
+  "net/url"
+  "encoding/xml"
+  "io/ioutil"
 )
 
 type Page struct {
   Name string
   DBStatus bool
-  }
+}
+
+type SearchResult struct {
+  Title string `xml:"title,attr"`
+  Author string `xml:"author,attr"`
+  Year string `xml:"hyr,attr"`
+  ID string `xml:"owi,attr"`
+}
 
 func main() {
-	template := template.Must(template.ParseFiles("templates/index.html"))
+  templates := template.Must(template.ParseFiles("templates/index.html"))
 
-	db, _ := sql.Open("sqlite3", "dev.db")
+  db, _ := sql.Open("sqlite3", "dev.db")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		p := Page{Name: "Jose"}
-		if name := r.FormValue("name"); name != ""{
-			p.Name = name
-		}
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    p := Page{Name: "Gopher", DBStatus: false}
+    if name := r.FormValue("name"); name != "" {
+      p.Name = name
+    }
 
-		p.DBStatus = db.Ping() == nil
+    p.DBStatus = db.Ping() == nil
 
-		if err := template.ExecuteTemplate(w, "index.html", p); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			}
-		})
+    if err := templates.ExecuteTemplate(w, "index.html", p); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+  })
 
-	fmt.Println(http.ListenAndServe(":8080", nil))
+  http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+    var results []SearchResult
+    var err error
+    
+    if results, err = search(r.FormValue("search")); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+
+    encoder := json.NewEncoder(w)
+    if err := encoder.Encode(results); err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+  })
+
+
+  fmt.Println(http.ListenAndServe(":8080", nil))
+}
+
+type ClassifyResponse struct {
+  Results []SearchResult `xml:"works>work"`
+}
+
+func search(query string) ([]SearchResult, error) {
+  var resp *http.Response
+  var err error
+  var c ClassifyResponse
+
+  if resp, err = http.Get("http://classify.oclc.org/classify2/Classify?title=" + url.QueryEscape(query) + "&summary=true"); err != nil {
+    return c.Results, err
+  }
+
+  defer resp.Body.Close()
+
+  var body []byte
+  if body, err = ioutil.ReadAll(resp.Body); err != nil {
+    return c.Results, err
+  }
+
+  if err = xml.Unmarshal(body, &c); err != nil {
+    return c.Results, err
+  }
+
+  return c.Results, nil
 }
